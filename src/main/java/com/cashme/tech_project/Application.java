fc9c6436de3c7c34535966983c9f2714373dea23
Augusto <augustoortolan02@gmail.com>
2024-06-30 18:29:00 -0300

    Init config of project

---
+++ src/main/java/com/cashme/tech_project/Application.java
@@ -0,0 +1,15 @@
+package com.cashme.tech_project;
+
+import com.cashme.tech_project.domain.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Generated
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
