5f8db2b2ffa46cbd5c595e1375c51408d2a76a39
Augusto <augustoortolan02@gmail.com>
2024-07-01 14:33:35 -0300

    Set banner spring boot off

--- src/main/java/com/cashme/tech_project/Application.java
+++ src/main/java/com/cashme/tech_project/Application.java
@@ -1,6 +1,7 @@
 package com.cashme.tech_project;
 
 import com.cashme.tech_project.domain.Generated;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		final var app = new SpringApplication(Application.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
 	}
 
 }
